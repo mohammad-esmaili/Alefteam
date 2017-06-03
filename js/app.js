24332e2fab312042d4206a307199e7eb9d64ffb3
Mohammad Esmaili <esmaili.programer@gmail.com>
2017-06-03 13:11:46 +0430

    add Inline Js code From Index File

--- js/app.js
+++ js/app.js
@@ -9,3 +9,52 @@ function setCoocki(cname,cvalue,days)
 
 	document.cookie = cname + " = " + cvalue + "; " + expires + "; path = " + location ;
 }
+
+
+
+
+/****** add code for responsive Menu *******/
+
+
+$(document).ready(function () {
+                $("#showMenu").hide();
+                $("#dogleMenu").click(function () {
+                    $("#showMenu").slideToggle();
+                }) ;
+
+                function windowsize() {
+
+                    var winsize= $(window).width();
+                    if(winsize <= "640")
+                    {
+                        $(".menuList").css("display","none");
+                        $("#dogleMenu").css("display","block");
+                    }
+                    else
+                    {
+                        $(".menuList").css("display","block");
+                        $("#dogleMenu").css("display","none");
+
+
+                    }
+
+                }
+
+				windowsize();
+
+                $(window).resize(function () {
+                    var winsize= $(window).width();
+                    if(winsize <= "640")
+                    {
+                       $(".menuList").css("display","none");
+                        $("#dogleMenu").css("display","block");
+                    }
+                    else
+                    {
+                        $(".menuList").css("display","block");
+                        $("#dogleMenu").css("display","none");
+
+                    }
+                });
+
+            });
